691ca4aef4847c01b7daa1e765d749d688a36988
CasaOasis <casaoasisgt@gmail.com>
2023-09-13 23:59:10 -0600

    Ready login

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+{
+  /* The following line can be included in your src/index.js or App.js file */
+}
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css'
+import Home from "./components/home/Home";
+import Login from "./components/login/Login";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [usuario, steUsuario] = useState(null); //Crear una variable de estado con dato null (Nada)
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {usuario ? <Home /> : <Login />}{" "}
+        {/*Si la variable de estado es null se redirigira a login ya que no hay un dato valido*/}
+        {/*Pero si, si tiene un dato valido, se ridirigira Home*/}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
 export default App
